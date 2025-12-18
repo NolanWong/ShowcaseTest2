4606b39ff3cb58f2f2e2cdead8f41aa6510136d1
ImperialOfficer <nolancwong@gmail.com>
2025-12-18 13:43:22 -0600

    slides

---
+++ script.js
@@ -0,0 +1,95 @@
+let slideIndex = [0];
+let slideIndex2 = [0];
+let slideIndex3 = [0];
+let slideIndex4 = [0];
+let LBModSlideIndex = [0];
+showSlides();
+showSlides2();
+showSlides3();
+showSlides4();
+showSlidesLBMod();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4500); // Every 1000 counts as a second
+}
+
+function showSlides2() {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("dot2");
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";  
+  dots2[slideIndex2-1].className += " active";
+  setTimeout(showSlides2, 4500); // Every 1000 counts as a second
+}
+
+function showSlides3() {
+  let i;
+  let slides3 = document.getElementsByClassName("mySlides3");
+  let dots3 = document.getElementsByClassName("dot3");
+  for (i = 0; i < slides3.length; i++) {
+    slides3[i].style.display = "none";  
+  }
+  slideIndex3++;
+  if (slideIndex3 > slides3.length) {slideIndex3 = 1}    
+  for (i = 0; i < dots3.length; i++) {
+    dots3[i].className = dots3[i].className.replace(" active", "");
+  }
+  slides3[slideIndex3-1].style.display = "block";  
+  dots3[slideIndex3-1].className += " active";
+  setTimeout(showSlides3, 4500); // Every 1000 counts as a second
+}
+
+function showSlides4() {
+  let i;
+  let slides4 = document.getElementsByClassName("mySlides4");
+  let dots4 = document.getElementsByClassName("dot4");
+  for (i = 0; i < slides4.length; i++) {
+    slides4[i].style.display = "none";  
+  }
+  slideIndex4++;
+  if (slideIndex4 > slides4.length) {slideIndex4 = 1}    
+  for (i = 0; i < dots4.length; i++) {
+    dots4[i].className = dots4[i].className.replace(" active", "");
+  }
+  slides4[slideIndex4-1].style.display = "block";  
+  dots4[slideIndex4-1].className += " active";
+  setTimeout(showSlides4, 4500); // Every 1000 counts as a second
+}
+
+function showSlidesLBMod() {
+  let i;
+  let slidesLBMod = document.getElementsByClassName("mySlidesLBMod");
+  let dots2 = document.getElementsByClassName("dotLBMod");
+  for (i = 0; i < slidesLBMod.length; i++) {
+    slidesLBMod[i].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slidesLBMod > slidesLBMod.length) {slidesLBMod = 1}    
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slidesLBMod[LBModSlideIndex-1].style.display = "block";  
+  dots2[LBModSlideIndex-1].className += " active";
+  setTimeout(showSlidesLBMod, 4500); // Every 1000 counts as a second
+}
